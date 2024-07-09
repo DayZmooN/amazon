b024078737bcd400ba3c1c10d0945d028d475a00
KrDevWeb <121158123+DayZmooN@users.noreply.github.com>
2024-07-09 16:30:47 +0200

    update security homepage login

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Récupérer tous les articles
+        $articleRepository = $entityManager->getRepository('App\Entity\Article');
+        $articles = $articleRepository->findAll();
+
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'articles' => $articles,
         ]);
     }
 }
